48193fe2c248201edc18bac9c76dd71b7db64824
antonioedb <antonioedb@gmail.com>
2025-06-10 10:01:14 -0300

    Correção da variael de ambiente

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,13 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-require('dotenv').config()
-
-module.exports = {
-  env: {
-    BACKEND_URL: process.env.BACKEND_URL,
-  },
-}
-
-
 export default nextConfig;
