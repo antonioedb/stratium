3195a3f4d1140ff807506198f9d2d82061e22313
antonioedb <antonioedb@gmail.com>
2025-06-11 13:14:34 -0300

    Correção nome stratium

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -189,7 +189,7 @@ export default function Home() {
                 />
               </div>
               <div>
-                <label className="block text-gray-700 text-sm font-bold mb-2">Saída na Sexta-feira: (do mes de vencimento)</label>
+                <label className="block text-gray-700 text-sm font-bold mb-2">Saída na sexta-feira: (do mês de vencimento)</label>
                 <select
                   {...register("friday_type")}
                   className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
